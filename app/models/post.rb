d992a561fd32c1e88ed31d2345939bf2eda9dbe5
Meisuleen Kased <meisuleen@gmail.com>
2019-10-07 20:18:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { is: 100 }, allow_blank: true
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
